706a166b7ad45609cd8e5c7a22b0c3283b1ec743
29araf <arafabul29@gmail.com>
2025-09-02 01:42:51 +0530

    dist folder added

---
+++ ApiTestAngular/dist/api-test-angular/server/assets-chunks/dashboard_index_html.mjs
@@ -0,0 +1,15 @@
+export default `<!DOCTYPE html><html lang="en" data-beasties-container><head>
+  <meta charset="utf-8">
+  <title>ApiTestAngular</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="styles-BBURD7AU.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-BBURD7AU.css"></noscript></head>
+<body><!--nghm-->
+  <app-root ng-version="19.2.14" ngh="1" ng-server-context="ssg"><router-outlet></router-outlet><app-dashboard ngh="0"><p>dashboard works!</p></app-dashboard><!----></app-root>
+<link rel="modulepreload" href="chunk-54Y7ET3T.js"><link rel="modulepreload" href="chunk-YNYT4YKT.js"><script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-3CWJFGXT.js" type="module"></script>
+<link rel="modulepreload" href="chunk-E62N7VD3.js">
+
+
+<script id="ng-state" type="application/json">{"__nghData__":[{},{"c":{"0":[{"i":"c2809702239","r":1}]}}]}</script></body></html>`;
